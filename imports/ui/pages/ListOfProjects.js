bc6f675bab20d0d4be2186ec5fef0877b62b2d70
Michael Lee <mlee@vellichorlabs.com>
2016-12-25 06:49:12 -0900

    Changelog
    * Added sweetalert CSS
    * Used a hack to make sure content stays in a grid by "filling up" the rest of the row with empty boxes
    * Had to use a pageId identifier to make sure the grid is properly offsetted depending on the page.

--- imports/ui/pages/ListOfProjects.js
+++ imports/ui/pages/ListOfProjects.js
@@ -3,6 +3,20 @@ import ProjectBox from '../components/ProjectBox.js';
 import CreateNewProjectForm from '../components/CreateNewProjectForm.js';
 
 export default class ListOfProjects extends Component {
+	fillEmptyRow() {
+		let offset, emptyArray = [];
+		if (this.props.pageId === 'main') {
+			offset = 5;
+		} else {
+			offset = 4;
+		}
+		let numberOfEmptyBoxes = offset - this.props.projects.length % offset;
+		for (i = 0; i < numberOfEmptyBoxes; i++) {
+			emptyArray.push('');
+		}
+		return emptyArray.map(each => <div className="empty-box"></div>);
+	}
+
 	render() {
 		return (
 			<section className="list-of-projects"> 
@@ -15,7 +29,8 @@ export default class ListOfProjects extends Component {
 						createdAt={project.createdAt}
 						total={project.total}
 						currentUser={this.props.currentUser} />
-				))} 
+				))}
+				{this.fillEmptyRow()}
 			</section>
 		);
 	}
