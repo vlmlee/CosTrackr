105332cde3c8a94b1eaad098304aef67688e92fc
Michael Lee <mlee@vellichorlabs.com>
2017-01-04 15:41:03 -0900

    Added some template methods to settings.

--- imports/ui/pages/Settings.js
+++ imports/ui/pages/Settings.js
@@ -1,5 +1,51 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Accounts } from 'meteor/accounts-base';
 
-const Settings = () => (
-	<div> This is the settings page. </div>
-);
+export default class Settings extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		};
+
+		this.handleChangeUsername = this.handleChangeUsername.bind(this);
+		this.handleChangePassword = this.handleChangePassword.bind(this);
+		this.handleChangeEmail = this.handleChangePassword.bind(this);
+		this.handleVerifyEmail = this.handleVerifyEmail.bind(this);
+		this.addAmazonAffiliate = this.addAmazonAffiliate.bind(this);
+	}
+
+	handleChangeUsername() {
+
+	}
+
+	handleChangePassword() {
+
+	}
+
+	handleChangeEmail() {
+
+	}
+
+	handleVerifyEmail() {
+
+	}
+
+	addAmazonAffiliate() {
+
+	}
+
+	render() {
+		return(
+			<section>
+				{ this.props.currentUser === Meteor.user().userId ? 
+					<h1>Show</h1>
+				: '' }
+			</section>
+		);
+	}
+};
+
+Settings.propTypes = {
+	currentUser: PropTypes.string.isRequired,
+};
